3167dd6f151d1b5387a5b6c064690f555760b28a
Rezo <rezokomakhidze1@gmail.com>
2023-07-08 16:39:50 +0400

    added necessary events

--- contracts/sharedWallet.sol
+++ contracts/sharedWallet.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.19;
+pragma solidity >=0.4.22 <=0.8.17;
 
 contract SharedWallet {
-    
+    event AccountCreated(address[] indexed owners, uint256 indexed id, uint256 timestamp);
+    event Deposit(address indexed user, uint256 indexed accountId, uint256 amount, uint256 timestamp);
+    event WithdrawRequested(address indexed user, uint256 indexed accountId, uint256 indexed withdrawId, uint256 amount, uint256 timestamp);
+    event Withdraw(uint256 indexed withdrawId, uint256 timestamp);
 }
